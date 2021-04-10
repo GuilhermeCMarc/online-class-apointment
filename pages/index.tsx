2c19edd1d504e5d749076623020d7c6a3ab32a43
Guilherme Marcondes <guisupre@gmail.com>
2021-04-10 11:55:15 -0300

    Start Project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="py-10">
+      <h1 className="text-5xl text-center text-accent">
+        Next.JS + TailwindCSS
+      </h1>
+    </div>
+  );
+};
+export default Home;
